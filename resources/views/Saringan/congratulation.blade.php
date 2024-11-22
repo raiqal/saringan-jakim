e3df5aed62b7b8827899def0892beb11e402a84e
raiqal <muhammadsyafiq0906@gmail.com>
2024-11-22 17:45:58 +0800

    update: shadow

--- resources/views/Saringan/congratulation.blade.php
+++ resources/views/Saringan/congratulation.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
                     <div class="card-header">
                         <h5>Congratulations!</h5>
                     </div>
--- resources/views/Saringan/create.blade.php
+++ resources/views/Saringan/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container ">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
                     <div class="card-header">
                         <div>
                             <h5>Registration For Participant</h5>
